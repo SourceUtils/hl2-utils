6cfed143b832e50fef45127aa5f6bc6c6341fc6c
TimePath <andrew.hardaker1995@gmail.com>
2014-06-02 11:20:28 +1000

    Updated diff test to read from text fields

--- src/main/java/com/timepath/hl2/VDFDiffTest.java
+++ src/main/java/com/timepath/hl2/VDFDiffTest.java
@@ -1,58 +1,80 @@
 package com.timepath.hl2;
 
+import com.timepath.steam.io.VDF;
 import com.timepath.steam.io.VDFNode;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * @author TimePath
  */
-class VDFDiffTest {
+class VDFDiffTest extends JFrame {
 
     private static final Logger LOG = Logger.getLogger(VDFDiffTest.class.getName());
+    private JTextArea text1, text2;
 
-    private VDFDiffTest() {}
+    private VDFDiffTest() {
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+        add(new JSplitPane() {{
+            setResizeWeight(.5);
+            setLeftComponent(new JScrollPane(text1 = new JTextArea() {{
+                setTabSize(4);
+                setText("\"A\" {\n" +
+                        "\t\"Modified\" {\n" +
+                        "\t\t\"Same\"\t\"yes\"\n" +
+                        "\t\t\"Similar\"\t\"one\"\n" +
+                        "\t\t\"Removed\"\t\"yes\"\n" +
+                        "\t}\n" +
+                        "\t\"Removed\" {}\n" +
+                        "\t\"Same\" {}\n" +
+                        "}\n");
+            }}));
+            setRightComponent(new JScrollPane(text2 = new JTextArea() {{
+                setTabSize(4);
+                setText("\"B\" {\n" +
+                        "\t\"Modified\" {\n" +
+                        "\t\t\"Same\"\t\"yes\"\n" +
+                        "\t\t\"Similar\"\t\"two\"\n" +
+                        "\t\t\"Added\"\t\"yes\"\n" +
+                        "\t}\n" +
+                        "\t\"New\" {}\n" +
+                        "\t\"Same\" {}\n" +
+                        "}\n");
+            }}));
+        }});
+        add(new JButton("Diff") {{
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(final ActionEvent e) {
+                    try {
+                        VDFNode n1 = VDF.load(new ByteArrayInputStream(text1.getText().getBytes(StandardCharsets.UTF_8)));
+                        VDFNode n2 = VDF.load(new ByteArrayInputStream(text2.getText().getBytes(StandardCharsets.UTF_8)));
+                        n1.getNodes().get(0).rdiff2(n2.getNodes().get(0));
+                    } catch(IOException ex) {
+                        LOG.log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+        }}, BorderLayout.SOUTH);
+        pack();
+        setLocationRelativeTo(null);
+    }
 
     public static void main(String... args) {
-        /**
-         * "VDF" {
-         * "Modified" {
-         * "Same"	"yes"
-         * "Similar"	"one"
-         * "Removed"	"yes"
-         * }
-         * "Removed" {}
-         * "Same" {}
-         * }
-         */
-        VDFNode n1 = new VDFNode("A");
-        /**
-         * "VDF" {
-         * "Modified" {
-         * "Same"	"yes"
-         * "Similar"	"two"
-         * "Added"	"yes"
-         * }
-         * "New" {}
-         * "Same" {}
-         * }
-         */
-        VDFNode n2 = new VDFNode("B");
-        VDFNode tmp = new VDFNode("Modified");
-        tmp.addAllProperties(new VDFNode.VDFProperty("Same", "yes"),
-                             new VDFNode.VDFProperty("Similar", "one"),
-                             new VDFNode.VDFProperty("Removed", "yes"));
-        n1.addNode(tmp);
-        n1.addNode(new VDFNode("Removed"));
-        n1.addNode(new VDFNode("Same"));
-        VDFNode tmp2 = new VDFNode("Modified");
-        tmp2.addAllProperties(new VDFNode.VDFProperty("Same", "yes"),
-                              new VDFNode.VDFProperty("Similar", "two"),
-                              new VDFNode.VDFProperty("Added", "yes"));
-        n2.addNode(tmp2);
-        n2.addNode(new VDFNode("New"));
-        n2.addNode(new VDFNode("Same"));
-        n1.rdiff2(n2);
-        //        Node.debugDiff(n1.rdiff(n2));
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new VDFDiffTest().setVisible(true);
+            }
+        });
     }
 }
