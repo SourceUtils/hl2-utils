59059090441fc77db82081085bbf9690ac63c275
TimePath <andrew.hardaker1995@gmail.com>
2014-09-29 21:00:52 +1000

    Sync with vfs

--- src/main/java/com/timepath/hl2/ArchiveHost.java
+++ src/main/java/com/timepath/hl2/ArchiveHost.java
@@ -3,7 +3,7 @@ package com.timepath.hl2;
 import com.timepath.hl2.io.bsp.BSP;
 import com.timepath.hl2.io.image.VTF;
 import com.timepath.steam.io.storage.ACF;
-import com.timepath.vfs.provider.Files;
+import com.timepath.vfs.provider.local.LocalFileProvider;
 import com.timepath.vfs.SimpleVFile;
 import com.timepath.vfs.SimpleVFile.MissingFileHandler;
 import com.timepath.vfs.server.ftp.FtpServer;
@@ -38,7 +38,7 @@ class ArchiveHost {
     public static void main(String[] args) {
         try {
             Class.forName(BSP.class.getName());
-            Files.registerMissingFileHandler(new MissingFileHandler() {
+            LocalFileProvider.registerMissingFileHandler(new MissingFileHandler() {
                 @Nullable
                 @Override
                 public SimpleVFile handle(@NotNull final SimpleVFile parent, @NotNull final String name) {
