5070bfa47b3b7b90a6a4e3dc067a7728631c3d51
TimePath <andrew.hardaker1995@gmail.com>
2015-04-01 18:41:53 +1100

    Fix ArchiveHost

--- src/main/kotlin/com/timepath/hl2/ArchiveHost.kt
+++ src/main/kotlin/com/timepath/hl2/ArchiveHost.kt
@@ -1,9 +1,20 @@
 package com.timepath.hl2
 
 import com.timepath.hl2.io.bsp.BSP
+import com.timepath.hl2.io.image.VTF
+import com.timepath.steam.io.storage.ACF
+import com.timepath.vfs.SimpleVFile
+import com.timepath.vfs.server.ftp.FtpServer
+import com.timepath.vfs.server.http.HttpServer
+import java.awt.image.RenderedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
+import java.lang.ref.SoftReference
 import java.util.logging.Level
 import java.util.logging.Logger
+import javax.imageio.ImageIO
 import kotlin.platform.platformStatic
 
 /**
@@ -12,82 +23,73 @@ import kotlin.platform.platformStatic
 object ArchiveHost {
 
     private val LOG = Logger.getLogger(javaClass<ArchiveHost>().getName())
-    //
     public platformStatic fun main(args: Array<String>) {
+        Class.forName(javaClass<BSP>().getName())
+        SimpleVFile.registerMissingFileHandler(object : SimpleVFile.MissingFileHandler {
+            override fun handle(parent: SimpleVFile, name: String): SimpleVFile? {
+                if (!name.endsWith(".png")) return null
+                val s = name.replace(".png", ".vtf")
+                val vtf = parent.get(s)
+                if (vtf == null) return null
+                return object : SimpleVFile() {
+                    override val name = name
+                    override val isDirectory = false
+                    private var data = SoftReference<ByteArray>(null)
+
+                    override fun openStream(): InputStream? {
+                        var arr = data.get()
+                        if (arr == null) {
+                            try {
+                                LOG.log(Level.INFO, "Converting {0}...", vtf)
+                                val vtfStream = vtf.openStream()
+                                if (vtfStream == null) {
+                                    return null
+                                }
+                                val v = VTF.load(vtfStream)
+                                if (v == null) {
+                                    return null
+                                }
+                                val image = v.getImage(Math.min(1, v.mipCount - 1))
+                                if (image == null) {
+                                    return null
+                                }
+                                val baos = ByteArrayOutputStream()
+                                ImageIO.write(image as RenderedImage, "png", baos)
+                                arr = baos.toByteArray()
+                                data = SoftReference(arr)
+                            } catch (e: IOException) {
+                                LOG.log(Level.SEVERE, null, e)
+                            } finally {
+                                LOG.log(Level.INFO, "Converted {0}", vtf)
+                            }
+                        }
+                        return ByteArrayInputStream(arr)
+                    }
+                }
+            }
+        })
+        val appID = 440
+        val acf = ACF.fromManifest(appID)
+        val files = acf.list()
         try {
-            Class.forName(javaClass<BSP>().getName())
-            //            SimpleVFile.registerMissingFileHandler(object : MissingFileHandler {
-            //                override fun handle(parent: SimpleVFile, name: String): SimpleVFile? {
-            //                    if (!name.endsWith(".png")) return null
-            //                    val s = name.replace(".png", ".vtf")
-            //                    val vtf = parent.get(s)
-            //                    if (vtf == null) return null
-            //                    return object : SimpleVFile() {
-            //                        private var data = SoftReference<ByteArray>(null)
-            //
-            //                        override fun getName(): String {
-            //                            return name
-            //                        }
-            //
-            //                        override fun isDirectory(): Boolean {
-            //                            return false
-            //                        }
-            //
-            //                        override fun openStream(): InputStream? {
-            //                            var arr = data.get()
-            //                            if (arr == null) {
-            //                                try {
-            //                                    LOG.log(Level.INFO, "Converting {0}...", vtf)
-            //                                    val v = VTF.load(vtf!!.openStream())
-            //                                    if (v == null) {
-            //                                        return null
-            //                                    }
-            //                                    val image = v!!.getImage(Math.min(1, v!!.getMipCount() - 1))
-            //                                    if (image == null) {
-            //                                        return null
-            //                                    }
-            //                                    val baos = ByteArrayOutputStream()
-            //                                    ImageIO.write(image as RenderedImage, "png", baos)
-            //                                    arr = baos.toByteArray()
-            //                                    data = SoftReference<ByteArray>(arr)
-            //                                } catch (e: IOException) {
-            //                                    LOG.log(Level.SEVERE, null, e)
-            //                                } finally {
-            //                                    LOG.log(Level.INFO, "Converted {0}", vtf)
-            //                                }
-            //                            }
-            //                            return ByteArrayInputStream(arr)
-            //                        }
-            //                    }
-            //                }
-            //            })
-            //            val appID = 440
-            //            val acf = ACF.fromManifest(appID)
-            //            val files = acf.list()
-            //            try {
-            //                val http = HttpServer()
-            //                http.addAll(files)
-            //                Thread(http).start()
-            //            } catch (ex: IOException) {
-            //                LOG.log(Level.SEVERE, null, ex)
-            //            }
-            //
-            //            try {
-            //                val ftp = FtpServer()
-            //                ftp.addAll(files)
-            //                Thread(ftp).start()
-            //            } catch (ex: IOException) {
-            //                LOG.log(Level.SEVERE, null, ex)
-            //            }
-            //
-            //            val fuse = FuseServer("test")
-            //            fuse.addAll(files)
-            //            Thread(fuse).start()
-        } catch (ex: ClassNotFoundException) {
-            LOG.log(Level.SEVERE, null, ex)
+            val http = HttpServer()
+            http.addAll(files.toList())
+            Thread(http).start()
         } catch (ex: IOException) {
             LOG.log(Level.SEVERE, null, ex)
         }
+
+        try {
+            val ftp = FtpServer()
+            ftp.addAll(files.toList())
+            Thread(ftp).start()
+        } catch (ex: IOException) {
+            LOG.log(Level.SEVERE, null, ex)
+        }
+
+        //        val fuse = FuseServer("test")
+        //        fuse.addAll(files)
+        //        Thread(fuse).start()
 
         //        SwingUtilities.invokeLater(object : Runnable {
         //            override fun run() {
