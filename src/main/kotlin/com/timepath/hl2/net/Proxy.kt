3939e3b76673ffd4ad356e0851302f00a93d9696
William Toohey <will@mon.im>
2015-07-07 23:32:18 +1000

    Clearer names

--- src/main/kotlin/com/timepath/hl2/net/Proxy.kt
+++ src/main/kotlin/com/timepath/hl2/net/Proxy.kt
@@ -11,10 +11,10 @@ fun main(args: Array<String>) {
     val proxyPort = args[0].toInt()
     val serverAddr = args[1].let { InetAddress.getByName(it) }
     val serverPort = args[2].toInt()
-    route(DatagramSocket(proxyPort), serverAddr, serverPort, {})
+    setupRoutes(DatagramSocket(proxyPort), serverAddr, serverPort, {})
 }
 
-fun route(router: DatagramSocket, serverAddr: InetAddress, serverPort: Int, handle: (DatagramPacket) -> Unit) {
+fun setupRoutes(proxy: DatagramSocket, serverAddr: InetAddress, serverPort: Int, handle: (DatagramPacket) -> Unit) {
     val writerS = "proxyclient"
     val readerS = "proxyserver"
     val clientS = "client"
@@ -24,17 +24,17 @@ fun route(router: DatagramSocket, serverAddr: InetAddress, serverPort: Int, hand
     var client: DatagramSocket
 
     val packet = packet()
-    route(packet, "$writerS <- $clientS", router, "$writerS -> $serverS", server) {
+    route(packet, "$writerS <- $clientS", proxy, "$writerS -> $serverS", server) {
         println("client is ${it.getAddress() to it.getPort()}")
         client = DatagramSocket() with { connect(it.getSocketAddress()) }
         handle(it)
         it.setAddress(null)
     }
-    route(writerS, packet, clientS, router, serverS, server) {
+    routeThread(writerS, packet, clientS, proxy, serverS, server) {
         handle(it)
         it.setAddress(null)
     }
-    route(readerS, packet(), serverS, server, clientS, router) {
+    routeThread(readerS, packet(), serverS, server, clientS, proxy) {
         it.setSocketAddress(client.getRemoteSocketAddress())
     }
 }
@@ -43,7 +43,7 @@ val SOURCE_MAX_BYTES = 1400
 
 fun packet() = ByteArray(SOURCE_MAX_BYTES) let { DatagramPacket(it, it.size()) }
 
-inline fun route(name: String, packet: DatagramPacket,
+inline fun routeThread(name: String, packet: DatagramPacket,
                  fromS: String, from: DatagramSocket,
                  toS: String, to: DatagramSocket,
                  @inlineOptions(InlineOption.ONLY_LOCAL_RETURN) handler: (DatagramPacket) -> Unit) {
